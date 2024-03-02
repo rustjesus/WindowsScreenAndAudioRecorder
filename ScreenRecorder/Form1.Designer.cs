97e5d85d2f8158c2ebb989401b3a25ffd0792403
rustjesus <yeric12345@gmail.com>
2024-03-01 20:45:15 -0600

    Add project files.

---
+++ ScreenRecorder/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ScreenRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.file_toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.recordScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopRecordScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleMicRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.micRecording_label1 = new System.Windows.Forms.Label();
+            this.Capturing_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file_toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(278, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // file_toolStripDropDownButton1
+            // 
+            this.file_toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.file_toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recordScreenToolStripMenuItem,
+            this.stopRecordScreenToolStripMenuItem,
+            this.toggleMicRecordToolStripMenuItem,
+            this.testToolStripMenuItem});
+            this.file_toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("file_toolStripDropDownButton1.Image")));
+            this.file_toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.file_toolStripDropDownButton1.Name = "file_toolStripDropDownButton1";
+            this.file_toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.file_toolStripDropDownButton1.Text = "File";
+            // 
+            // recordScreenToolStripMenuItem
+            // 
+            this.recordScreenToolStripMenuItem.Name = "recordScreenToolStripMenuItem";
+            this.recordScreenToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.recordScreenToolStripMenuItem.Text = "Start Capture";
+            this.recordScreenToolStripMenuItem.Click += new System.EventHandler(this.recordScreenToolStripMenuItem_Click_1);
+            // 
+            // stopRecordScreenToolStripMenuItem
+            // 
+            this.stopRecordScreenToolStripMenuItem.Name = "stopRecordScreenToolStripMenuItem";
+            this.stopRecordScreenToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.stopRecordScreenToolStripMenuItem.Text = "Stop Capture";
+            this.stopRecordScreenToolStripMenuItem.Click += new System.EventHandler(this.stopRecordScreenToolStripMenuItem_Click);
+            // 
+            // toggleMicRecordToolStripMenuItem
+            // 
+            this.toggleMicRecordToolStripMenuItem.Name = "toggleMicRecordToolStripMenuItem";
+            this.toggleMicRecordToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.toggleMicRecordToolStripMenuItem.Text = "Toggle Mic Capture";
+            this.toggleMicRecordToolStripMenuItem.Click += new System.EventHandler(this.toggleMicRecordToolStripMenuItem_Click);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.testToolStripMenuItem.Text = "Itch";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // micRecording_label1
+            // 
+            this.micRecording_label1.AutoSize = true;
+            this.micRecording_label1.Location = new System.Drawing.Point(12, 54);
+            this.micRecording_label1.Name = "micRecording_label1";
+            this.micRecording_label1.Size = new System.Drawing.Size(93, 13);
+            this.micRecording_label1.TabIndex = 1;
+            this.micRecording_label1.Text = "Mic Recording On";
+            // 
+            // Capturing_label
+            // 
+            this.Capturing_label.AutoSize = true;
+            this.Capturing_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Capturing_label.Location = new System.Drawing.Point(12, 30);
+            this.Capturing_label.Name = "Capturing_label";
+            this.Capturing_label.Size = new System.Drawing.Size(61, 13);
+            this.Capturing_label.TabIndex = 2;
+            this.Capturing_label.Text = "Capturing";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(753, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "v0.2.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Records one audio source and screen at same time.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 108);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Capturing_label);
+            this.Controls.Add(this.micRecording_label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton file_toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem recordScreenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopRecordScreenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleMicRecordToolStripMenuItem;
+        private System.Windows.Forms.Label micRecording_label1;
+        private System.Windows.Forms.Label Capturing_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ScreenRecorder/Form1.cs
@@ -0,0 +1,254 @@
+﻿
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Accord.Video.FFMPEG;
+using System.Collections.Generic;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using Accord.Audio;
+using NAudio.Midi;
+using Newtonsoft.Json;
+
+namespace ScreenRecorder
+{
+    public partial class Form1 : Form
+    {
+        private int fps = 30;
+        private int bitRate = 100;
+        private bool recordMic = false;
+
+
+
+        //awake
+        public Form1()
+        {
+            InitializeComponent();
+            LoadRecordMiceSettingsFromJson();
+            if (recordMic)
+            {
+                micRecording_label1.Text = "Audio Recording Mic";
+            }
+            else
+            {
+                micRecording_label1.Text = "Audio Recording Desktop";
+            }
+            Capturing_label.ForeColor = Color.Red;
+            Capturing_label.Text = "NOT Recording.";
+        }
+        //start
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SaveRecordMiceSettingsToJson()
+        {
+            var settings = new { RecordMic = recordMic };
+            var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+            var path = Path.Combine(Application.StartupPath, "recordMic.json");
+            File.WriteAllText(path, json);
+        }
+        private void LoadRecordMiceSettingsFromJson()
+        {
+            var path = Path.Combine(Application.StartupPath, "recordMic.json");
+            if (File.Exists(path))
+            {
+                var json = File.ReadAllText(path);
+                var settings = JsonConvert.DeserializeObject<dynamic>(json);
+                recordMic = settings.RecordMic;
+
+            }
+        }
+
+
+        //MICROPHONE RECORDER
+        private WaveFormat waveFormatMic;
+        private WaveFileWriter writerMic;
+        private WaveInEvent waveInMic;
+
+        private void StartMicRecording()
+        {
+            // Create a new wave format for the audio input
+            waveFormatMic = new WaveFormat(44100, 16, 2);
+
+            // Create a new wave file to save the recorded audio
+            writerMic = new WaveFileWriter("output_Mic.wav", waveFormatMic);
+
+            // Create a new audio capture device
+            waveInMic = new WaveInEvent();
+
+            // Set the audio capture device's input format to the wave format we created
+            waveInMic.WaveFormat = waveFormatMic;
+
+            // Define the callback function that will be called whenever audio data is available
+            waveInMic.DataAvailable += (sender, audioArgs) =>
+            {
+                // Write the audio data to the wave file
+                writerMic.Write(audioArgs.Buffer, 0, audioArgs.BytesRecorded);
+            };
+
+            // Start capturing audio
+            waveInMic.StartRecording();
+        }
+
+        private void StopMicRecording()
+        {
+            // Stop capturing audio
+            waveInMic.StopRecording();
+
+            // Dispose of the wave file writer to save the recorded audio
+            writerMic.Dispose();
+        }
+
+        //DESKTOP AUDIO RECORDER
+        private WasapiLoopbackCapture waveIn;
+        private WaveFileWriter writer;
+
+        public void StartRecordDesktopAudio()
+        {
+            waveIn = new WasapiLoopbackCapture();
+            writer = new WaveFileWriter("output_Desktop.wav", waveIn.WaveFormat);
+            waveIn.DataAvailable += (s, e) =>
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+            };
+            waveIn.RecordingStopped += (s, e) =>
+            {
+                writer.Dispose();
+                waveIn.Dispose();
+            };
+            waveIn.StartRecording();
+        }
+
+        public void EndRecordDesktopAudio()
+        {
+            waveIn.StopRecording();
+        }
+
+        //DESKTOP VIDEO RECORDING
+        private Rectangle screenBounds;
+        private Bitmap screenBitmap;
+        private Graphics screenGraphics;
+        private VideoFileWriter videoWriter;
+        private string videoFilePath;
+        private DateTime startTime;
+        private int frameCount;
+        private Thread screenCaptureThread;
+
+        public void StartRecording()
+        {
+            // Set up the screen capture
+            screenBounds = Screen.PrimaryScreen.Bounds;
+            screenBitmap = new Bitmap(screenBounds.Width, screenBounds.Height);
+            screenGraphics = Graphics.FromImage(screenBitmap);
+
+            // Set up the video writer
+            videoWriter = new VideoFileWriter();
+            videoFilePath = "screen_capture.avi";
+            videoWriter.Open(videoFilePath, screenBounds.Width, screenBounds.Height, fps, VideoCodec.MPEG4, bitRate * 1000000);
+
+            // Set up the recording variables
+            //recordingTimeSeconds = 10;
+            startTime = DateTime.Now;
+            frameCount = 0;
+
+            // Start the screen capture loop in a separate thread
+            screenCaptureThread = new Thread(ScreenCaptureLoop);
+            screenCaptureThread.Start();
+        }
+
+        public void StopRecording()
+        {
+            // Stop the screen capture loop and wait for it to finish
+            screenCaptureThreadRunning = false;
+            screenCaptureThread.Join();
+
+            // Close the video writer
+            videoWriter.Close();
+
+            // Display a message when finished
+            MessageBox.Show("Screen capture saved to " + videoFilePath);
+        }
+
+        private volatile bool screenCaptureThreadRunning = true;
+
+        private void ScreenCaptureLoop()
+        {
+            while (screenCaptureThreadRunning && (DateTime.Now - startTime).TotalSeconds < double.PositiveInfinity)
+            {
+                // Capture the screen and add it to the video
+                screenGraphics.CopyFromScreen(screenBounds.X, screenBounds.Y, 0, 0, screenBounds.Size);
+                videoWriter.WriteVideoFrame(screenBitmap);
+
+                // Measure the time it takes to capture and write a frame
+                TimeSpan frameTime = DateTime.Now - startTime;
+                double actualFps = (++frameCount) / frameTime.TotalSeconds;
+
+                // Calculate the sleep time based on the actual frame rate
+                int sleepTime = (int)(1000f / actualFps);
+
+                // Sleep for the calculated time
+                //Thread.Sleep(sleepTime);
+            }
+        }
+
+        //*********************************input**************************************************
+        private void recordScreenToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            StartRecording();
+            if (recordMic)
+            {
+                StartMicRecording();
+                //RecordMic();
+            }
+            else
+            {
+                StartRecordDesktopAudio();
+            }
+            Capturing_label.ForeColor = Color.LimeGreen;
+            Capturing_label.Text = "Recording!";
+        }
+        private void stopRecordScreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StopRecording();
+            if (recordMic)
+            {
+                StopMicRecording();
+                //RecordMic();
+            }
+            else
+            {
+                EndRecordDesktopAudio();
+            }
+            Capturing_label.ForeColor = Color.Red;
+            Capturing_label.Text = "NOT Recording.";
+        }
+
+        private void toggleMicRecordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            recordMic = !recordMic;
+            SaveRecordMiceSettingsToJson();
+            if (recordMic)
+            {
+                micRecording_label1.Text = "Audio Capturing Mic";
+            }
+            else
+            {
+                micRecording_label1.Text = "Audio Capturing Desktop";
+            }
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Specify the URL to open
+            string url = "https://manbearpigman.itch.io/notepad-on-crack";
+
+            // Start a new process to open the default browser and navigate to the URL
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
